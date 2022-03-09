8ebb279fa93fb61ee57679ae9e74c173791f2728
hexios <hiskying@icloud.com>
2022-03-09 11:33:59 +0800

    Initial commit

---
+++ js/ly.js
@@ -0,0 +1,24 @@
+//页面加载   获取全部信息
+$(function(){
+    $.ajax({
+        type: "POST",
+        url: "demo.json",
+        dataType: "json",
+        addBox(result){
+        }
+    });
+    $.get("demo.json",function(result){
+        //result数据添加到box容器中;
+        addBox(result);
+    });
+});
+function addBox(result){
+    //result是一个集合,所以需要先遍历
+    $.each(result,function(index,obj){
+        $("#box").append("<div align='center'>"+
+            "<div class='p1 p'>"+obj['PersonName']+"</div>"+
+            "<div class='p2 p'>"+obj['Date']+"</div>"+
+            "<div class='p3 p'>"+obj['Content']+"</div>"+
+            "</div>");
+    });
+}
